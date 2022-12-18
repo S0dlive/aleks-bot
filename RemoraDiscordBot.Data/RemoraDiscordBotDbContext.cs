1dcd04577c530467a269c59fa035a84966ffe218
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-12-18 20:45:03 +0100

    feat(data): passing the connection string through env variables

---
+++ RemoraDiscordBot.Data/RemoraDiscordBotDbContext.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace RemoraDiscordBot.Data;
+
+public class RemoraDiscordBotDbContext
+    : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection")
+                               ?? throw new InvalidOperationException("Connection string not found.");
+
+        optionsBuilder.UseSqlite(connectionString);
+    }
+}
