e1c68486ff8b6a406ecd52b101be95dd19679644
Alexis Chân Gridel <alexis.gridel@pm.me>
2022-12-30 22:56:01 +0100

    feat: fabulous creatures

--- RemoraDiscordBot.Bot/Commands/DiagnosticCommands.cs
+++ RemoraDiscordBot.Bot/Commands/DiagnosticCommands.cs
@@ -4,9 +4,14 @@
 
 using System.ComponentModel;
 using System.Drawing;
+using Microsoft.Extensions.Logging;
+using OneOf;
 using Remora.Commands.Attributes;
 using Remora.Commands.Groups;
+using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.API.Objects;
+using Remora.Discord.Commands.Feedback.Messages;
 using Remora.Discord.Commands.Feedback.Services;
 using Remora.Results;
 
@@ -16,11 +21,17 @@ public sealed class DiagnosticCommands
     : CommandGroup
 {
     private readonly FeedbackService _feedbackService;
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<DiagnosticCommands> _logger;
 
     public DiagnosticCommands(
-        FeedbackService feedbackService)
+        FeedbackService feedbackService,
+        HttpClient httpClient,
+        ILogger<DiagnosticCommands> logger)
     {
         _feedbackService = feedbackService;
+        _httpClient = httpClient;
+        _logger = logger;
     }
 
     [Command("hello")]
@@ -28,14 +39,36 @@ public sealed class DiagnosticCommands
     public async Task<IResult> HelloCommand(
         [Description("The following message that the bot will send")]
         string message)
+    
     {
+        //TODO: Refactor to service
+        var response = await _httpClient.GetAsync("http://localhost:5106/api/v1/Egg");
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            _logger.LogError("Failed to get the egg from the API");
+            return Result.FromSuccess();
+        }
+
+        var content = await response.Content.ReadAsStreamAsync();
+
+        var fileData = new FileData("egg.png", content, "image/png");
+
         return (Result) await _feedbackService.SendContextualEmbedAsync(
             new Embed
             {
                 Title = "Hello!",
                 Description = message,
-                Colour = Color.Green
+                Colour = Color.Green,
+                Image = new EmbedImage("attachment://egg.png")
             },
-            ct: CancellationToken);
+            new FeedbackMessageOptions
+            {
+                Attachments = new[]
+                {
+                    OneOf<FileData, IPartialAttachment>.FromT0(fileData)
+                }
+            },
+            CancellationToken);
     }
 }
