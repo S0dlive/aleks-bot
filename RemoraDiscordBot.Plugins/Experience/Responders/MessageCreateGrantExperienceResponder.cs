f17083e394e10a6dcc64665ee522ba0884a229a7
Alexis C. Gridel <contact@agdl.dev>
2023-01-08 19:04:54 +0100

    feat(GrantExperienceAmountToUserCommand): signature with guildId

--- RemoraDiscordBot.Plugins/Experience/Responders/MessageCreateGrantExperienceResponder.cs
+++ RemoraDiscordBot.Plugins/Experience/Responders/MessageCreateGrantExperienceResponder.cs
@@ -2,7 +2,6 @@
 // Licensed under the GNU General Public License v3.0.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Immutable;
 using MediatR;
 using Remora.Discord.API.Abstractions.Gateway.Events;
 using Remora.Discord.Gateway.Responders;
@@ -31,7 +30,9 @@ public class MessageCreateGrantExperienceResponder
         var wordsCount = gatewayEvent.Content.Split(' ').Distinct().Count();
         var xpEarned = CalculateExperience(messageLength, wordsCount);
 
-        await _mediator.Send(new GrantExperienceAmountToUserCommand(instigator.ID, gatewayEvent.ChannelID ,xpEarned), ct);
+        await _mediator.Send(
+            new GrantExperienceAmountToUserCommand(instigator.ID, gatewayEvent.GuildID.Value, gatewayEvent.ChannelID,
+                xpEarned), ct);
 
         return Result.FromSuccess();
     }
