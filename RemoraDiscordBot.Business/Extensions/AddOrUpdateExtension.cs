b552cde7839ba73d86b646fcde3039421718c3d6
Alexis C. Gridel <contact@agdl.dev>
2023-01-29 10:30:12 +0100

    feat(business): add add or update <T> method extension

---
+++ RemoraDiscordBot.Business/Extensions/AddOrUpdateExtension.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace RemoraDiscordBot.Business.Extensions;
+
+public static class AddOrUpdateExtension
+{
+    public static async Task AddOrUpdateAsync<T>(
+        this DbSet<T> dbSet,
+        T entity,
+        Expression<Func<T, bool>> predicate,
+        CancellationToken cancellationToken = default)
+        where T : class
+    {
+        var existing = await dbSet.FirstOrDefaultAsync(predicate, cancellationToken);
+
+        if (existing == null)
+        {
+            await dbSet.AddAsync(entity, cancellationToken);
+        }
+        else
+        {
+            dbSet.Attach(entity);
+            dbSet.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
