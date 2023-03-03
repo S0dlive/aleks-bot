1afb586d88fd5e9780d18d7bb5cc1cbb575574e7
S0dlive <sandlive662@gmail.com>
2023-03-03 17:07:14 +0100

    feat(ticket): TicketDetail.cs.

---
+++ RemoraDiscordBot.Data.Domain/Ticket/TicketDetail.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RemoraDiscordBot.Data.Domain.Ticket;
+
+public class TicketDetails
+{ 
+    [Key]
+    public string TicketId { get; set; }
+    public long GuildId { get; set; }
+    public long ChannelId { get; set; }
+    public long ModeratorId { get; set; }
+    public long UserId { get; set; }
+}
